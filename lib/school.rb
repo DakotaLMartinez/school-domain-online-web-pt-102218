d7e9a49beada4c3fee54bed17e6dafd62288d571
Dakota Martinez <dakotaleemusic@gmail.com>
2018-12-03 23:40:39 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
-# code here!
+require 'pry'
+
+class School
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def roster
+    @roster
+  end
+  
+  def add_student(name, grade)
+    self.roster[grade] ||= []
+    self.roster[grade] << name
+  end
+  
+  def grade(grade)
+    self.roster[grade]
+  end 
+  
+  def sort
+    new_hash = {}
+    @roster.reduce({}) do |memo, grade_and_names|
+      grade, names = grade_and_names
+      memo[grade] = names.sort
+      memo
+    end
+    # new_hash
+   # binding.pry
+  end
+
+end
